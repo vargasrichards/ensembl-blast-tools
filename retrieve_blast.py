4ca094c469e3afc6566d4e723e5f388396e11f69
vargasrichards <ar2185@cam.ac.uk>
2024-07-21 12:05:41 +0100

    moving docs to special folder

--- retrieve_blast.py
+++ retrieve_blast.py
@@ -1,5 +1,5 @@
 import subprocess
-import get_blast
+import requester
 print("Finished HTTPS BLAST results retrieval")
 print("Calling cleaning script...")
 subprocess.call(['bash', './process.sh'])
