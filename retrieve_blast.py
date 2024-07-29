4e08f464f79a31e7fcf61bf94a47313b05cc18ab
vargasrichards <Alexis.VargasRichards@niab.com>
2024-07-29 15:53:25 +0100

    reorganised main script

--- retrieve_blast.py
+++ retrieve_blast.py
@@ -1,31 +1,34 @@
 # Main script called by user.
-# 
 # A. Vargas Richards, 22.07.2024 - 
 
 import subprocess
 import requester
+import make_fasta
+import summariser 
+import call_msa
+
+# First we get the BLAST results via HTTPS
 requester.fetch_blast(
 varieties = ['claire', 'paragon', 'cadenza', 'robigus'],
 species = 'Triticum_aestivum_',
 batch_id = 'dH2MUh3d5qVTSKJc',
 first = 23302378,
 last = 23302413)
-import make_fasta
-import summariser 
-import call_msa
+
+
 print("Finished HTTPS BLAST results retrieval")
 print("Calling cleaning script...")
 subprocess.call(['bash', './process.sh'])
-print("----")
+print("----------------------------")
 print("DONE CLEANING FILES")
-print("----")
+print("----------------------------")
 print("Generating summary files")
 summariser.summarise_all("BLAST-results")
 print("Generating FASTA files...")
 make_fasta.make_all("BLAST-results")
-print("-----")
+print("----------------------------")
 print("Finished making FASTA files")
-print("-----")
+print("----------------------------")
 print("Proceeding with Multiple Sequence Alignment using MUSCLE")
 call_msa.call_msa("BLAST-results")
 print("Finished MSA")
