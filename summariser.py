9c3595f0f7aff31260b317fe99b461888366e39b
vargasrichards <ar2185@cam.ac.uk>
2024-08-05 13:14:07 +0100

    updated gitignore

--- summariser.py
+++ summariser.py
@@ -4,6 +4,8 @@
 import pandas as pd, os
 
 def summarise(file):
+    #print(f'File tuple is {file}')
+    #parent_directory, filename = file.split('/')
     summary_file = str('summary' + file)
     file = str('BLAST-results' + '/' + file)
     with open(file, 'r') as input:
@@ -22,9 +24,11 @@ def summarise_all(directory):
 
 def tabulate(summary_file): # produces a sorted table of the results
     summary_table = pd.DataFrame(columns=["Variety", "Identifier", "Score", "E-value"])
+    
     with open(summary_file) as summary:
         for line in summary:
             parts = [part for part in line.split(' ') if part]
+            #print(parts)
             variety = parts[0]
             scaff_id = variety.split(':')[1]
             variety_name = scaff_id.split('_')[0]
@@ -33,15 +37,17 @@ def tabulate(summary_file): # produces a sorted table of the results
             row = pd.DataFrame([[variety_name, scaff_id, score, eval]], 
                                columns=["Variety", "Identifier", "Score", "E-value"])
             summary_table = pd.concat([summary_table, row], ignore_index=True)
+
     sorted_summary = summary_table.sort_values("E-value") # sort by E_values
     os.makedirs('CSVs', exist_ok=True)  
     csv_name = 'CSVs/' + summary_file + '.csv'
     sorted_summary.to_csv(csv_name)  
+    #print(sorted_summary)
     return sorted_summary
 
 
 def tabulate_all(directory):
-    for root, dirs, files in os.walk(directory, topdown=True):
+    for root,dirs,files in os.walk(directory, topdown=True):
         for file in files:
             if 'summary' in file: # checking for the correct file type...
                 tabulate(file)
